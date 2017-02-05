6ea4061630f1cf331349d4cf3c289c0f82d2ef75
Luis Martins <luis.pinto.martins@gmail.com>
2017-02-05 06:52:55 -0800

    Added necessary docker files, with working windows docker files Added docker install script

---
+++ install-docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo apt-get update
+
+sudo apt-get -y install \
+	curl \
+    linux-image-extra-$(uname -r) \
+	linux-image-extra-virtual
+
+sudo apt-get -y install \
+	apt-transport-https \
+	ca-certificates
+
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
+sudo apt-get update
+
+sudo apt-get -y install \
+	docker-engine
+
+# list docker available versions
+# apt-cache madison docker-engine
+
+sudo apt-get -y install \
+	docker-engine=1.12.3-0~xenial
