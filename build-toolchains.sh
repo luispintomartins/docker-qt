d0f4eb9d6b7a4f3082d9c5ad447b8132b212bbfa
lmartins <luis.pinto.martins@efacec.com>
2017-05-14 11:10:15 +0100

    Updated build-toolchains script
    Signed-off-by: lmartins <luis.pinto.martins@efacec.com>

--- build-toolchains.sh
+++ build-toolchains.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 CURRENT_PATH=$PWD
 
+QT_VERSION="5.8"
+
+# STATIC
+
 # windows i386
-cd $CURRENT_PATH/qt/5.8/windows/i386/static     && docker build -t qt5.windows.i386.static:5.8 .
+cd $CURRENT_PATH/qt/$QT_VERSION/windows/i386/static     && docker build -t qt.windows.i386.static:$QT_VERSION .
 # windows amd64
-cd $CURRENT_PATH/qt/5.8/windows/amd64/static    && docker build -t qt5.windows.amd64.static:5.8 .
+cd $CURRENT_PATH/qt/$QT_VERSION/windows/amd64/static    && docker build -t qt.windows.amd64.static:$QT_VERSION .
 # linux i386
-cd $CURRENT_PATH/qt/5.8/linux/i386/static       && docker build -t qt5.linux.i386.static:5.8 .
+cd $CURRENT_PATH/qt/$QT_VERSION/linux/i386/static       && docker build -t qt.linux.i386.static:$QT_VERSION .
 # linux amd64
-cd $CURRENT_PATH/qt/5.8/linux/amd64/static      && docker build -t qt5.linux.amd64.static:5.8 .
+cd $CURRENT_PATH/qt/$QT_VERSION/linux/amd64/static      && docker build -t qt.linux.amd64.static:$QT_VERSION .
 # linux armhf
-cd $CURRENT_PATH/qt/5.8/linux/armhf/static      && docker build -t qt5.linux.armhf.static:5.8 .
-# linux armel
-cd $CURRENT_PATH/qt/5.8/linux/armel/static      && docker build -t qt5.linux.armel.static:5.8 .
+cd $CURRENT_PATH/qt/$QT_VERSION/linux/armhf/static      && docker build -t qt.linux.armhf.static:$QT_VERSION .
+
+# SHARED
+
+# windows i386
+cd $CURRENT_PATH/qt/$QT_VERSION/windows/i386/shared     && docker build -t qt.windows.i386.shared:$QT_VERSION .
+# windows amd64
+cd $CURRENT_PATH/qt/$QT_VERSION/windows/amd64/shared    && docker build -t qt.windows.amd64.shared:$QT_VERSION .
+# linux i386
+cd $CURRENT_PATH/qt/$QT_VERSION/linux/i386/shared       && docker build -t qt.linux.i386.shared:$QT_VERSION .
+# linux amd64
+cd $CURRENT_PATH/qt/$QT_VERSION/linux/amd64/shared      && docker build -t qt.linux.amd64.shared:$QT_VERSION .
+# linux armhf
+cd $CURRENT_PATH/qt/$QT_VERSION/linux/armhf/shared      && docker build -t qt.linux.armhf.shared:$QT_VERSION .
