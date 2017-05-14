7122e974b5b176c0c95cbcfb8f86df70206b60af
lmartins <luis.pinto.martins@efacec.com>
2017-05-14 22:54:52 +0100

    Created Qt 5.8 shared amd64 toolchain

---
+++ qt/5.8/windows/amd64/shared/buildscript.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+dpkg --add-architecture amd64
+apt-get update
+apt-get install -y --no-install-recommends \
+    autoconf \
+    automake \
+    autopoint \
+    binutils \
+    bison \
+    build-essential \
+    ca-certificates \
+    cmake \
+    debhelper \
+    devscripts \
+    fakeroot \
+    flex \
+    gcc \
+    git \
+    gperf \
+    intltool \
+    libgdk-pixbuf2.0-dev \
+    libffi-dev \
+    libgmp-dev \
+    libmpc-dev \
+    libmpfr-dev \
+    libtool \
+    libtool-bin \
+    libz-dev \
+    openssl \
+    patch \
+    pkg-config \
+    p7zip-full \
+    ruby \
+    scons \
+    subversion \
+    texinfo \
+    unzip \
+    wget
+
+mkdir -p /build
+cd /build
+
+git clone https://github.com/mxe/mxe.git
+
+cd /build/mxe
+
+# git checkout -b mxe_5_8 27facf1c6fcb7973781568dde9b0c1072e28f669
+
+make MXE_TARGETS='x86_64-w64-mingw32.shared' \
+    qtbase \
+    qt5 \
+    qtcharts \
+    qtdatavis3d \
+    qtimageformats \
+    qtlocation \
+    qtmultimedia \
+    qtserialport \
+    qtsvg \
+    qttranslations \
+    qtvirtualkeyboard \
+    qtwinextras
+
+update-alternatives --install /usr/bin/qmake qmake /build/mxe/usr/bin/x86_64-w64-mingw32.shared-qmake-qt5 10
+
+update-alternatives --install /usr/bin/mingw-gcc mingw-gcc /build/mxe/usr/bin/x86_64-w64-mingw32.shared-gcc 10
+update-alternatives --install /usr/bin/mingw-g++ mingw-g++ /build/mxe/usr/bin/x86_64-w64-mingw32.shared-g++ 10
+update-alternatives --install /usr/bin/mingw-windres mingw-windres /build/mxe/usr/bin/x86_64-w64-mingw32.shared-windres 10
+
+update-alternatives --install /usr/bin/x86_64-w64-mingw32.shared-gcc x86_64-w64-mingw32.shared-gcc /build/mxe/usr/bin/x86_64-w64-mingw32.shared-gcc 10
+update-alternatives --install /usr/bin/x86_64-w64-mingw32.shared-g++ x86_64-w64-mingw32.shared-g++ /build/mxe/usr/bin/x86_64-w64-mingw32.shared-g++ 10
+update-alternatives --install /usr/bin/x86_64-w64-mingw32.shared-windres x86_64-w64-mingw32.shared-windres /build/mxe/usr/bin/x86_64-w64-mingw32.shared-windres 10
+
+mkdir p /src
