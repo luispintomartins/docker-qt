2d3bad7f6e95196bf2215a98be097ceed0a0832a
Luis Martins <luis.pinto.martins@gmail.com>
2017-05-18 22:07:01 +0100

    Reduced message output.

--- clear-docker.sh
+++ clear-docker.sh
@@ -3,8 +3,10 @@
 # See: https://github.com/docker/docker/issues/3182
 
 # Delete 'exited' containers
-docker rm -v $(docker ps -a -q -f status=exited)
+docker rm -v $(docker ps -a -q -f status=exited) &> /dev/null
 # Delete 'dangling' images
-docker rmi $(docker images -f "dangling=true" -q)
+docker rmi $(docker images -f "dangling=true" -q) &> /dev/null
 # Delete 'dangling' volumes
-docker volume rm $(docker volume ls -qf dangling=true)
+docker volume rm $(docker volume ls -qf dangling=true) &> /dev/null
+# done
+echo "Done."
